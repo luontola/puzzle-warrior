ac8b26a09ca7f825563a7393a5d51c0764b14749
Esko Luontola <esko.luontola@gmail.com>
2008-02-24 18:07:40 +0000

    todo

--- src/test/java/net/orfjackal/puzzlewarrior/BoardSpec.java
+++ src/test/java/net/orfjackal/puzzlewarrior/BoardSpec.java
@@ -552,12 +552,14 @@ public class BoardSpec extends Specification<Board> {
 }
 
 // TODO: game ending (event?)
+// TODO: ticker (reset tick timer on manual move down)
 // TODO: GUI
 // TODO: counting points (event?)
 // TODO: counter gems (start from 5, need to drop 5 gems before turns into a normal gem)
 // TODO: counter gems explode if near to a crash gem
 // TODO: sending counter gem groups to the opponent
 // TODO: grouping gems to rectangles
+// TODO: multiplayer mode
 
 // normal gem
 // crash gem = explosive block
